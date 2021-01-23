261d7f083afa4f2ae2b2da6aa66c230675cad1c4
Lukasz <lukasz-madej@live.com>
2021-01-23 17:49:28 +0100

    New version started

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>LM</h1>
+      <p>Development in progress</p>
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default HomePage;
